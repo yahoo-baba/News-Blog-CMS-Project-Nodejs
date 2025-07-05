e88a41d6e03dacf2e2d1638b5f79d2d0886c4178
yahoo-baba <helloyahoobaba@gmail.com>
2025-07-05 14:35:36 +0530

    frontend code

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -6,19 +6,81 @@ const userModel = require('../models/User');
 const commentModel = require('../models/Comment');
 
 const index = async (req,res) => {
-  res.render('index')
+  const news = await newsModel.find()
+                        .populate('category',{'name':1, 'slug':1})
+                        .populate('author','fullname')
+                        .sort({createdAt: -1})
+
+  const categoriesInUse = await newsModel.distinct('category')   
+  const categories = await categoryModel.find({'_id':{$in:categoriesInUse}})                   
+  // res.json({ news, categories })
+  res.render('index', { news, categories })
  }
+
+
 const articleByCategories = async (req,res) => {
-  res.render('category')
+  const category = await categoryModel.findOne({ slug: req.params.name });
+
+  if (!category) {
+    return res.status(404).send('Category not found');
+  }
+
+  const news = await newsModel.find({ category: category._id })
+                        .populate('category',{'name':1, 'slug':1})
+                        .populate('author','fullname')
+                        .sort({createdAt: -1})
+
+  const categoriesInUse = await newsModel.distinct('category')   
+  const categories = await categoryModel.find({'_id':{$in:categoriesInUse}})   
+
+  res.render('category', { news, categories, category })
  }
+
 const singleArticle = async (req,res) => { 
-  res.render('single')
+  const singleNews = await newsModel.findById(req.params.id)
+                        .populate('category',{'name':1, 'slug':1})
+                        .populate('author','fullname')
+                        .sort({createdAt: -1})
+
+  const categoriesInUse = await newsModel.distinct('category')   
+  const categories = await categoryModel.find({'_id':{$in:categoriesInUse}}) 
+
+  res.render('single', { singleNews, categories })
 }
+
 const search = async (req,res) => {
-  res.render('search')
+  const searchQuery = req.query.search
+
+  const news = await newsModel.find({
+                      $or: [
+                        { title: { $regex: searchQuery, $options: 'i' } },
+                        { content: { $regex: searchQuery, $options: 'i' } }
+                      ]
+                    })
+                    .populate('category',{'name':1, 'slug':1})
+                    .populate('author','fullname')
+                    .sort({createdAt: -1})
+
+  const categoriesInUse = await newsModel.distinct('category')   
+  const categories = await categoryModel.find({'_id':{$in:categoriesInUse}}) 
+
+  res.render('search', { news, categories, searchQuery })
  }
 const author = async (req,res) => { 
-  res.render('author')
+  const author = await userModel.findOne({ _id: req.params.name });
+  if (!author) {
+    return res.status(404).send('Author not found');
+  }
+
+  const news = await newsModel.find({ author: req.params.name })
+                        .populate('category',{'name':1, 'slug':1})
+                        .populate('author','fullname')
+                        .sort({createdAt: -1})
+  // res.json(news)
+  const categoriesInUse = await newsModel.distinct('category')   
+  const categories = await categoryModel.find({'_id':{$in:categoriesInUse}}) 
+
+  res.render('author', { news, categories, author })
 }
 const addComment = async (req,res) => { }
 
