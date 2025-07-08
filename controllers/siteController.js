a0255a171de8d80b174e4322efafe3ada14cb5e5
yahoo-baba <helloyahoobaba@gmail.com>
2025-07-08 21:26:53 +0530

    News Blog frontend pagination code

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -5,26 +5,37 @@ const newsModel = require('../models/News');
 const userModel = require('../models/User');
 const settingModel = require('../models/Setting');
 const commentModel = require('../models/Comment');
+const paginate = require('../utils/paginate')
 
 const index = async (req,res) => {
-  const news = await newsModel.find()
-                        .populate('category',{'name':1, 'slug':1})
-                        .populate('author','fullname')
-                        .sort({createdAt: -1})
 
-  // res.json({ news})
-  res.render('index', { news})
+  const paginatedNews = await paginate(newsModel, {}, 
+                                      req.query, {
+                                      populate: [
+                                        { path: 'category', select: 'name slug' },
+                                        { path: 'author', select: 'fullname' }
+                                      ],   
+                                      sort: '-createdAt' })
+
+  // res.json({ paginatedNews })
+  res.render('index', { paginatedNews , query: req.query})
  }
 
 
 const articleByCategories = async (req,res) => {
   const category = await categoryModel.findOne({ slug: req.params.name });
-
   if (!category) {
     return res.status(404).send('Category not found');
   }
+  const paginatedNews = await paginate(newsModel, { category: category._id }, 
+                                      req.query, {
+                                      populate: [
+                                        { path: 'category', select: 'name slug' },
+                                        { path: 'author', select: 'fullname' }
+                                      ],   
+                                      sort: '-createdAt' })
 
-  res.render('category', { news, category })
+  res.render('category', { paginatedNews, category, query: req.query })
  }
 
 const singleArticle = async (req,res) => { 
@@ -39,18 +50,21 @@ const singleArticle = async (req,res) => {
 const search = async (req,res) => {
   const searchQuery = req.query.search
 
-  const news = await newsModel.find({
-                      $or: [
-                        { title: { $regex: searchQuery, $options: 'i' } },
-                        { content: { $regex: searchQuery, $options: 'i' } }
-                      ]
-                    })
-                    .populate('category',{'name':1, 'slug':1})
-                    .populate('author','fullname')
-                    .sort({createdAt: -1})
+  const paginatedNews = await paginate(newsModel, {
+                              $or: [
+                                { title: { $regex: searchQuery, $options: 'i' } },
+                                { content: { $regex: searchQuery, $options: 'i' } }
+                              ]
+                            }, 
+                            req.query, {
+                            populate: [
+                              { path: 'category', select: 'name slug' },
+                              { path: 'author', select: 'fullname' }
+                            ],   
+                            sort: '-createdAt' })
 
 
-  res.render('search', { news, searchQuery })
+  res.render('search', { paginatedNews, searchQuery, query: req.query })
  }
 const author = async (req,res) => { 
   const author = await userModel.findOne({ _id: req.params.name });
@@ -58,7 +72,16 @@ const author = async (req,res) => {
     return res.status(404).send('Author not found');
   }
 
-  res.render('author', { news, author })
+  const paginatedNews = await paginate(newsModel, { author: req.params.name  }, 
+                                      req.query, {
+                                      populate: [
+                                        { path: 'category', select: 'name slug' },
+                                        { path: 'author', select: 'fullname' }
+                                      ],   
+                                      sort: '-createdAt' })
+
+
+  res.render('author', { paginatedNews, author, query: req.query })
 }
 const addComment = async (req,res) => { }
 
---
+++ utils/paginate.js
@@ -0,0 +1,33 @@
+
+const paginate = async (model, query = {}, reqQuery = {}, options = {}) => {
+  const { page = 1, limit = 2, sort = '-createdAt' } = reqQuery
+
+  const paginationOptions = {
+    page : parseInt(page), 
+    limit : parseInt(limit),  
+    sort,
+    ...options
+  }
+
+  try {
+  const result = await model.paginate(query, paginationOptions)
+
+  return {
+    data: result.docs,
+    prevPage: result.prevPage,
+    nextPage: result.nextPage,
+    hasNextPage: result.hasNextPage,
+    hasPrevPage: result.hasPrevPage,
+    currentPage: result.page,
+    counter:result.pagingCounter,
+    limit: result.limit,
+    totalDocs: result.totalDocs,
+    totalPages: result.totalPages
+  }
+
+  }catch(error){
+    console.log('Pagination Error', error.message)
+  }
+}
+
+module.exports = paginate
